2795181ee3ffccc06720981e43b70565039b108a
asifalam515 <asibulalam515@gmail.com>
2026-05-31 21:25:32 +0600

    Homepage updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import GreenhouseLogin from "@/components/auth/GreenhouseLogin"
+
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <GreenhouseLogin />
     </div>
   )
 }
